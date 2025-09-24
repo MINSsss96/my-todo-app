73409c8db9b43731d67ea3a621e44e41dda04af5
MINSsss96 <minsue0422@naver.com>
2025-09-24 10:33:40 +0900

    기본 setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        // 프론트에서 /api로 호출하면 -> http://localhost:8080/api
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    }
+
+  }
 })
